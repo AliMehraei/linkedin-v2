388d2d70f3c959cc6628e5631de38e6648890d15
Ali Mehraei <ali.mehraei@gmail.com>
2022-10-19 12:21:46 +0330

    fix: fix get auth URL function in linkedInCOnfig.

--- src/Http/Controllers/Auth/LinkedInConfig.php
+++ src/Http/Controllers/Auth/LinkedInConfig.php
@@ -3,10 +3,6 @@
 namespace alimehraei\LinkedInAllInOne\Http\Controllers\Auth;
 
 
-use Carbon\Carbon;
-use alimehraei\LinkedInAllInOne\LinkedInAllInOne;
-use alimehraei\LinkedInAllInOne\ZohoToken;
-
 class LinkedInConfig
 {
 
@@ -14,14 +10,10 @@ class LinkedInConfig
     {
 
         $client_id = config('linkedin-v2.client_id');
-        $secret_key = config('linkedin-v2.client_secret');
-        $z_url = config('linkedin-v2.accounts_url');
-        $z_return_url = config('linkedin-v2.redirect_uri');
-        $z_api_url = config('linkedin-v2.api_base_url');
-        $z_current_user_email = config('linkedin-v2.current_user_email');
-        $z_oauth_scope = config('linkedin-v2.oauth_scope');
+        $return_url = config('linkedin-v2.redirect_uri');
+        $oauth_scope = config('linkedin-v2.oauth_scope');
 
-        return "$z_url/oauth/v2/auth?scope=$z_oauth_scope&client_id=$client_id&response_type=code&access_type=offline&redirect_uri=$z_return_url";
+        return "https://www.linkedin.com/oauth/v2/authorization?scope=$oauth_scope&client_id=$client_id&response_type=code&redirect_uri=$return_url";
 
     }
 
